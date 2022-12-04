0035fce664df95d88c467912b44f258d942bda6c
Dietmar Moritz <dietmar.moritz@willhaben.at>
2022-12-04 20:45:33 +0100

    fix wrong commit of day-03-1

--- 2022/day-03-1.py
+++ 2022/day-03-1.py
@@ -6,7 +6,7 @@ def find_item(line: str) -> int:
     second = line[int(len(line) / 2):]
     for i in line:
         if i in second:
-            points = ord(i) - (ord('a') if ord(i) > ord('3') else (ord('A') - 26)) + 1
+            points = ord(i) - (ord('a') if ord(i) > ord('Z') else (ord('A') - 26)) + 1
             print(f'{line} -> {second} -> {i} -> {points}')
             return points
 
