cd60bf17755d12612bf29881d9043f21e38cde4e
Dietmar Moritz <dietmar.moritz@willhaben.at>
2022-12-05 22:22:54 +0100

    add day-05-1

---
+++ 2022/day-05-1.py
@@ -0,0 +1,31 @@
+import re
+
+with open('day-05.data') as f:
+    data = [line.rstrip('\n') for line in f]
+
+stack_numbers_line = 0
+while data[stack_numbers_line][1] != '1':
+    stack_numbers_line += 1
+
+stacks_count = int((len(data[stack_numbers_line]) + 2) / 4)
+
+stacks = []
+for i in range(0, stacks_count):
+    stacks.append([])
+    for j in range(stack_numbers_line, 0, -1):
+        char = data[j - 1][i * 4 + 1]
+        if char != ' ':
+            stacks[i].append(char)
+
+for i in range(stack_numbers_line + 2, len(data)):
+    line = data[i]
+    print(line)
+    count, from_stack, to_stack = [int(i) for i in (re.search('move (\d+) from (\d) to (\d)', line).groups())]
+    for i in range(0, count):
+        stacks[to_stack - 1].append(stacks[from_stack - 1].pop())
+
+result = ''
+for i in range(0, stacks_count):
+    result += stacks[i][-1]
+
+print(result)
