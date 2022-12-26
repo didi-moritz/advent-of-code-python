e4a00fd581c843ebda2d41a7bb793f1416124664
didi-moritz <didi.moritz@gmail.com>
2022-12-26 22:29:07 +0100

    add day-20-1 (2022)

---
+++ 2022/day-20-1.py
@@ -0,0 +1,33 @@
+with open('day-20.data') as f:
+    data = [int(line.rstrip('\n')) for line in f]
+
+n = data.copy()
+
+length = len(n)
+
+print(n)
+
+for i in data:
+    pos = n.index(i)
+    n = n[pos + 1:length] + n[0:pos]
+
+    steps = i % len(n)
+
+    while steps < 0:
+        steps += len(n)
+
+    move_dir = 1 if i > 0 else 0
+
+    n = n[0:steps] + [i] + n[steps:len(n)]
+
+index_0 = n.index(0)
+
+r_1 = n[(index_0 + 1000) % length]
+r_2 = n[(index_0 + 2000) % length]
+r_3 = n[(index_0 + 3000) % length]
+
+print(r_1)
+print(r_2)
+print(r_3)
+print('=' * 4)
+print(r_1 + r_2 + r_3)
