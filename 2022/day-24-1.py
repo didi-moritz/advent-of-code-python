c290c5f672f6c7e803df605938841ee97c39e5dc
didi-moritz <didi.moritz@gmail.com>
2023-01-01 14:33:24 +0100

    optimize day-24-1 (2022)

--- 2022/day-24-1.py
+++ 2022/day-24-1.py
@@ -7,8 +7,8 @@ sys.setrecursionlimit(10000)
 with open('day-24.data') as f:
     data = [line.rstrip('\n') for line in f]
 
-height = len(data)
-width = len(data[0])
+height = len(data) - 2
+width = len(data[0]) - 2
 
 
 class Point:
@@ -38,8 +38,8 @@ class Point:
         self.y = point.y
 
 
-START = Point(1, 0)
-END = Point(width - 2, height - 1)
+START = Point(0, -1)
+END = Point(width - 1, height)
 
 
 class Blizzard:
@@ -48,16 +48,10 @@ class Blizzard:
         self.pos = pos
         self.move = move
 
-    def move_it(self):
-        self.pos.move(self.move)
-        if self.pos.x == width - 1:
-            self.pos.x = 1
-        elif self.pos.x == 0:
-            self.pos.x = width - 2
-        elif self.pos.y == height - 1:
-            self.pos.y = 1
-        elif self.pos.y == 0:
-            self.pos.y = height - 2
+    def get_pos(self, minute):
+        point = self.pos.add(Point(self.move.x * minute, self.move.y * minute))
+        point.set(Point(point.x % width, point.y % height))
+        return point
 
 
 N = Point(0, -1)
@@ -68,7 +62,10 @@ WAIT = Point(0, 0)
 
 ALL_DIRS = [S, E, N, W, WAIT]
 
-start_blizzards: list[Blizzard] = []
+blizzards_h = {}
+blizzards_v = {}
+blizzards_all = []
+already_visited = {}
 
 start_pos = START
 
@@ -78,51 +75,74 @@ def possible_go_tos(pos):
 
 
 def load():
-    for y in range(1, len(data) - 1):
-        line = data[y]
-        for x in range(1, len(line) - 1):
-            if data[y][x] == '.':
+    for y in range(height):
+        blizzards_h[y] = []
+    for x in range(width):
+        blizzards_v[x] = []
+
+    for y in range(height):
+        for x in range(width):
+            if data[y + 1][x + 1] == '.':
                 continue
 
             point = Point(x, y)
 
-            if data[y][x] == '>':
-                start_blizzards.append(Blizzard(point, E))
-            elif data[y][x] == '<':
-                start_blizzards.append(Blizzard(point, W))
-            elif data[y][x] == '^':
-                start_blizzards.append(Blizzard(point, N))
+            if data[y + 1][x + 1] == '>':
+                blizzards_h[y].append(Blizzard(point, E))
+            elif data[y + 1][x + 1] == '<':
+                blizzards_h[y].append(Blizzard(point, W))
+            elif data[y + 1][x + 1] == '^':
+                blizzards_v[x].append(Blizzard(point, N))
             else:
-                start_blizzards.append(Blizzard(point, S))
+                blizzards_v[x].append(Blizzard(point, S))
+
+    for blizzards in blizzards_v.values():
+        for blizzard in blizzards:
+            blizzards_all.append(blizzard)
+
+    for blizzards in blizzards_h.values():
+        for blizzard in blizzards:
+            blizzards_all.append(blizzard)
 
 
 load()
 
 
-def print_map(pos, blizzards, count):
+def print_map(pos, minute):
     os.system('clear')
-    print(f'count: {count}')
+    print(f'minute: {minute}')
     print(f'best: {min_steps}')
+
+    m = []
     for y in range(height):
+        m.append([])
         for x in range(width):
+            m[y].append(' ')
+
+    for blizzard in blizzards_all:
+        point = blizzard.get_pos(minute)
+        if blizzard.move == N:
+            c = '^'
+        elif blizzard.move == S:
+            c = 'v'
+        elif blizzard.move == E:
+            c = '>'
+        else:
+            c = '<'
+        m[point.y][point.x] = c
+
+    for y in range(-1, height + 1):
+        for x in range(-1, width + 1):
             point = Point(x, y)
-            c = '.'
 
             if point == pos:
                 c = '\033[1;32mX\033[0m'
-            elif x == 0 or x == width - 1 or y == 0 or y == height - 1 and point != START and point != END:
+            elif point == START or point == END:
+                c = ' '
+            elif x == -1 or x == width or y == -1 or y == height:
                 c = '#'
             else:
-                for blizzard in blizzards:
-                    if blizzard.pos == point:
-                        if blizzard.move == N:
-                            c = '^'
-                        elif blizzard.move == S:
-                            c = 'v'
-                        elif blizzard.move == E:
-                            c = '>'
-                        else:
-                            c = '<'
+                c = m[y][x]
 
             print(c, end='')
 
@@ -134,36 +154,70 @@ min_steps = 100000
 iteration = 0
 
 
-def action(pos: Point, blizzards: list[Blizzard], count: int):
+def action(pos: Point, minute: int):
     global min_steps
     global iteration
 
-    if count >= min_steps:
+    if minute >= min_steps:
         return
+
+    if pos in already_visited:
+        if minute in already_visited[pos]:
+            return
+    else:
+        already_visited[pos] = []
+
+    already_visited[pos].append(minute)
 
     iteration += 1
 
-    if not iteration % 1000:
-        print_map(pos, blizzards, count)
+    if not iteration % 10000:
+        print_map(pos, minute)
 
     go_tos = possible_go_tos(pos)
 
-    new_blizzards = copy.deepcopy(blizzards)
+    min_x = width
+    max_x = 0
+    min_y = height
+    max_y = 0
 
-    for blizzard in new_blizzards:
-        blizzard.move_it()
-        if blizzard.pos in go_tos:
-            del go_tos[blizzard.pos]
+    go_to_keys = copy.deepcopy(list(go_tos.keys()))
+    for go_to in go_to_keys:
+        if go_to == END:
+            min_steps = min(min_steps, minute)
+            return
+
+        if (0 > go_to.x or go_to.x >= width) or (0 > go_to.y or go_to.y >= height) and go_to != START:
+            del go_tos[go_to]
+
+    for go_to in go_tos:
+        min_x = min(min_x, go_to.x)
+        max_x = max(max_x, go_to.x)
+        min_y = min(min_y, go_to.y)
+        max_y = max(max_y, go_to.y)
+
+    for y in range(min_y, max_y + 1):
+        if 0 <= y < height:
+            for blizzard in blizzards_h[y]:
+                point = blizzard.get_pos(minute)
+                if point in go_tos:
+                    del go_tos[point]
+
+    for x in range(min_x, max_x + 1):
+        if 0 <= x < width:
+            for blizzard in blizzards_v[x]:
+                point = blizzard.get_pos(minute)
+                if point in go_tos:
+                    del go_tos[point]
 
     for go_to in go_tos:
         if go_to == END:
-            min_steps = min(min_steps, count)
+            min_steps = min(min_steps, minute)
             break
 
-        if 0 < go_to.x < width - 1 and (0 < go_to.y < height - 1 or go_to == START):
-            action(go_to, new_blizzards, count + 1)
+        action(go_to, minute + 1)
 
 
-action(start_pos, start_blizzards, 1)
+action(start_pos, 1)
 
 print(min_steps)
