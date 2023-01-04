159499332c68e07359a8d1728721b50b2fbc1623
didi-moritz <didi.moritz@gmail.com>
2023-01-04 20:29:18 +0100

    add day-10-2 (2015)

---
+++ 2015/day-10-2.py
@@ -0,0 +1,19 @@
+number = '1113122113'
+
+for i in range(50):
+    current = 0
+    count = 0
+    result = ''
+    for j in range(len(number) + 1):
+        c = number[j] if j < len(number) else 0
+        if c == current:
+            count += 1
+        else:
+            if count > 0:
+                result += f'{count}{current}'
+            current = c
+            count = 1
+
+    number = result
+
+print(len(number))
