d4c7373d21be399de088c36facbf663340004ee8
didi-moritz <didi.moritz@gmail.com>
2023-01-08 21:00:38 +0100

    add day-18-2 (2015) - Game of Life

---
+++ 2015/day-18-2.py
@@ -0,0 +1,81 @@
+import os
+import time
+
+with open('day-18.data') as f:
+    data = [line.rstrip('\n') for line in f]
+
+width = len(data[0])
+height = len(data)
+
+lights = []
+
+
+def load():
+    for y in range(height):
+        lights.append([])
+        for x in range(width):
+            lights[y].append(data[y][x] == '#' or ((x == 0 or x == width - 1) and (y == 0 or y == height - 1)))
+
+
+load()
+
+
+def get_new_state(x, y):
+    if (x == 0 or x == width - 1) and (y == 0 or y == height - 1):
+        return True
+
+    on = 0
+    for check_y in range(max(0, y - 1), min(height, y + 2)):
+        for check_x in range(max(0, x - 1), min(height, x + 2)):
+            if x == check_x and y == check_y:
+                continue
+            if lights[check_y][check_x]:
+                on += 1
+            if on > 3:
+                return False
+
+    if lights[y][x]:
+        return 2 <= on <= 3
+    else:
+        return on == 3
+
+
+def print_map():
+    os.system('clear')
+    for y in range(height):
+        for x in range(width):
+            if lights[y][x]:
+                print('\033[96m\033[1m#\033[0m', end='')
+            else:
+                print(' ', end='')
+        print()
+
+
+count = 0
+STEPS = 100
+
+
+def action():
+    global lights
+    global count
+
+    for step in range(STEPS):
+        new_lights = []
+        for y in range(height):
+            new_lights.append([])
+            for x in range(width):
+                new_lights[y].append(get_new_state(x, y))
+        lights = new_lights
+        print_map()
+        print(step)
+        time.sleep(0.1)
+
+    for y in range(height):
+        for x in range(width):
+            if lights[y][x]:
+                count += 1
+
+
+action()
+
+print(count)
