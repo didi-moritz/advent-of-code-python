3dc9580543c98d42adf14260ee6205915060e2b4
didi-moritz <didi.moritz@gmail.com>
2023-02-16 20:48:14 +0100

    add day-07-1 (2016)

---
+++ 2016/day-07-1.py
@@ -0,0 +1,34 @@
+import re
+
+with open('day-07.data') as f:
+    data = [line.rstrip('\n') for line in f]
+
+sequences_pattern = re.compile(r'(^|])([^[]+)')
+hypernet_sequences_pattern = re.compile(r'\[([^]]+)]')
+abba_pattern = re.compile(r'(.)(.)\2\1')
+
+result = 0
+
+
+def is_abba(text):
+    matches = list(filter(lambda i: i[0] != i[1], abba_pattern.findall(part)))
+    return len(matches) > 0
+
+
+for line in data:
+    parts = list(map(lambda i: i[1], sequences_pattern.findall(line)))
+
+    found = False
+
+    for part in parts:
+        found = found or is_abba(part)
+
+    if found:
+        hypernet_parts = hypernet_sequences_pattern.findall(line)
+        for part in hypernet_parts:
+            found = found and not is_abba(part)
+
+    if found:
+        result += 1
+
+print(result)
